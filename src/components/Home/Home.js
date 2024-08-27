b0054b31d26435c655616996db305ac534815f5e
claupalac <clausspal97@gmail.com>
2024-08-26 20:17:36 -0400

    add badges

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Title from "../title/Title";
 import PresentationText from "../title/PresentationText";
 import './Home.css';
-// import BadgesBar from "../BadgesBar/BadgesBar";
+import BadgesBar from "../BadgesBar/BadgesBar";
 
 function Home() {
+    const badges = ['Badge 1', "Badge 2", "Badge 3"];
     return(
         <div className="home">
         <Title/>
-        <PresentationText></PresentationText>
-        {/*<BadgesBar></BadgesBar>*/}
+        <BadgesBar badges={badges}></BadgesBar>
         <PresentationText></PresentationText>
         <PresentationText></PresentationText>
         <PresentationText></PresentationText>
--- src/components/title/PresentationText.js
+++ src/components/title/PresentationText.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './PresentationText.css';
 
 function PresentationText() {
+  const presentationTextRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = presentationTextRef.current;
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+          element.classList.add('animate');
+          window.removeEventListener('scroll', handleScroll);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="presentation-text">
+    <div className="presentation-text" ref={presentationTextRef}>
       <h2>About Me</h2>
       <p>
         I am a <b>passionate developer</b> with experience in building web applications using modern technologies.
         I enjoy solving complex problems and continuously learning new skills to improve my craft.
       </p>
-
     </div>
   );
 }
